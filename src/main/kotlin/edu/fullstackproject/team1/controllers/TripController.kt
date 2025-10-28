b922f5cd7085c13d4fc6f09f7c80632582b2cb82
Macarena Alvear <macarena.alvear1901@alumnos.ubiobio.cl>
2025-10-28 17:43:30 -0300

    T1FSPL-71 Delete Itinerary

--- src/main/kotlin/edu/fullstackproject/team1/controllers/TripController.kt
+++ src/main/kotlin/edu/fullstackproject/team1/controllers/TripController.kt
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -35,5 +37,15 @@ class TripController(
 	): ResponseEntity<TripsListResponse> {
 		val response = tripService.getUserTrips(user.username)
 		return ResponseEntity.ok(response)
+	}
+
+	//DELETE
+	@DeleteMapping("/itineraries/{id}")
+	fun deleteItinerary(
+		@AuthenticationPrincipal user: UserDetails,
+		@PathVariable id: Long
+	): ResponseEntity<Map<String,String>>{
+		tripService.deleteTrip(user.username, id)
+		return ResponseEntity.ok(mapOf("message" to "Itineraries deleted successfully"))
 	}
 }
