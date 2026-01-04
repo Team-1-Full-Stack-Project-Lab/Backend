aaedee0f0bf96efd13a52ba7ab2d43f35d1fe017
Carlos Badilla <carlosbadillac@outlook.com>
2026-01-04 02:09:10 -0300

    refactor: remove ktlint from project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 	kotlin("plugin.serialization") version "2.2.0"
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
 	id("org.flywaydb.flyway") version "11.15.0"
 	kotlin("plugin.jpa") version "2.2.10"
 }
